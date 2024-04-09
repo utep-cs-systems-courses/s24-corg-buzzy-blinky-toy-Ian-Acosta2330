d9ff232894c94505d746f58ceba50b3eb9386a51
Ian-Acosta2330 <ian2330acosta@gmail.com>
2024-04-08 18:49:44 -0600

    currently trying to add buttons to do something

--- project/toy/main.c
+++ project/toy/main.c
@@ -3,18 +3,28 @@
 #include "led.h"
 #include "buzzer.h"
 
+
 #define SW1 BIT3
-#define SW2 BIT3
-//#define SW3 BIT2
-//#define SW4 BIT3
-#define SWITCH_A SW1
-#define SWITCH_B SW2
-//#define SWITCHES SW1, SW2, SW3, SW4
+#define SW2 BIT0
+#define SW3 BIT1
+#define SW4 BIT2
+#define SW5 BIT3
+
+//#define SWITCH_A SW1
+//#define SWITCH_B SW2
+
+//4 and 5 for sounds
+#define SWITCH_L SW2, SW3, SW4, SW5
+//#define SWITCH_S SW4, SW5
+#define SWITCH_1 SW1
 
 int main(void) {
   P1DIR |= LEDS;
   P1OUT &= ~LEDS;
 
+  P2DIR |= LEDS;
+  P2OUT &= ~LEDS;
+  
   /*
   P2DIR |= LEDS
   P2OUT &= ~LEDS;
@@ -24,31 +34,29 @@ int main(void) {
 
   buzzer_init();
   
-  P2DIR |= LEDS; //setup the leds
-  P2OUT &= ~LEDS; //turn off red led
-
-  P1REN |= SWITCH_A;
-  P1IE |= SWITCH_A;
-  P1OUT |= SWITCH_A;
-  P1DIR &= ~SWITCH_A;
+  P1REN |= SWITCH_1;
+  P1IE |= SWITCH_1;
+  P1OUT |= SWITCH_1;
+  P1DIR &= ~SWITCH_1;
   
-  P2REN |= SWITCH_B;
-  P2IE |= SWITCH_B;
-  P2OUT |= SWITCH_B;
-  P2DIR &= ~SWITCH_B;
+  P2REN |= SWITCH_L;
+  P2IE |= SWITCH_L;
+  P2OUT |= SWITCH_L;
+  P2DIR &= ~SWITCH_L;
 
   or_sr(0x18);
 }
 
 void switch_interrupt_handler() {
   char p1val = P1IN;
+  
   char p2val = P2IN;
 
-  P1IES |= (p1val & SWITCH_A);
-  P1IES &= (p1val | ~SWITCH_A);
+  P1IES |= (p1val & SWITCH_1);
+  P1IES &= (p1val | ~SWITCH_1);
   
-  P2IES |= (p2val & SWITCH_B);
-  P2IES &= (p2val | ~SWITCH_B);
+  P2IES |= (p2val & SWITCH_L);
+  P2IES &= (p2val | ~SWITCH_L);
 
   //S1 turns on red led
   if (p1val & SW1) {
@@ -62,14 +70,14 @@ void switch_interrupt_handler() {
   }
   
   if(p2val & SW2) {
-    //P2OUT |= LED_GREEN;
-    //P2OUT &= ~LED_RED;
-    buzzer_set_period(0);
+    P2OUT |= LED_GREEN;
+    P2OUT &= ~LED_RED;
+    // buzzer_set_period(0);
   } else {
-    //P2OUT &= ~LED_GREEN;
-    buzzer_set_period(800);
+    P2OUT &= ~LED_GREEN;
+    //buzzer_set_period(800);
   }
-  /*
+  
   if(p2val & SW3) {
     buzzer_set_period(0);
   }else {
@@ -79,20 +87,27 @@ void switch_interrupt_handler() {
   if(p2val & SW4) {
     buzzer_set_period(0);
   } else {
+    buzzer_set_period(800);
+  }
+
+  if(p2val & SW5) {
+    buzzer_set_period(0);
+  } else {
     buzzer_set_period(1000);
-  }*/
+  }
+  
 }
 
 void __interrupt_vec(PORT1_VECTOR) Port_1() {
-  if(P1IFG & SWITCH_A) {
-    P1IFG &= ~SWITCH_A;
+  if(P1IFG & SWITCH_1) {
+    P1IFG &= ~SWITCH_1;
     switch_interrupt_handler();
   }
 }
 
 void __interrupt_vec(PORT2_VECTOR) Port_2() {
-  if (P2IFG & SWITCH_B) {
-    P2IFG &= ~SWITCH_B;
+  if (P2IFG & SWITCH_L) {
+    P2IFG &= ~SWITCH_L;
     switch_interrupt_handler();
   }
   
